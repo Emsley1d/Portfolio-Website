a957ab20d3bfc00ce4647712aa55fd562ea78ccb
emsleyd <emsleyd@gmail.com>
2022-11-10 17:25:07 +0000

    fullpage is not a constructor error

---
+++ fullpage.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+    anchors: ['about', 'projects', 'skills'],
+    sectionsColor: ['#222831', 'orange', '#222831', '#ADD8E6'],
+    
+    licenseKey: '5K9K9-3NWS9-OYK68-O7J2J-YOFXN'
+
+  });
+  
+  // document.querySelector('.show-content').addEventListener('click', showContent);
+  
+  // function showContent(){
+  //   document.querySelector('.extra-content').classList.toggle('active');
+  // }
