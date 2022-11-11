352755d3ce20433e9e3dba86cb1467033c58f576
emsleyd <emsleyd@gmail.com>
2022-11-11 16:07:22 +0000

    fullpage.js styling updates

--- fullpage.js
+++ fullpage.js
@@ -1,7 +1,13 @@
-new fullpage('#fullpage', {
-    anchors: ['about', 'projects', 'skills'],
-    sectionsColor: ['#222831', 'orange', '#222831', '#ADD8E6'],
-    
-    licenseKey: '5K9K9-3NWS9-OYK68-O7J2J-YOFXN'
+var myFullpage = new fullpage('#fullpage', {
 
-  });
+  licenseKey: '5K9K9-3NWS9-OYK68-O7J2J-YOFXN',
+  anchors: ['about', 'projects', 'skills'],
+  navigation: 'true',
+  scrollingSpeed: 800,
+  fixedElements: '#nav-home',
+  credits: { enabled: false, label: 'Made with fullPage.js', position: 'right'},
+  fixedElements: '#header',
+  navigationPosition: 'right',
+  fitToSection: 'true',
+
+});
